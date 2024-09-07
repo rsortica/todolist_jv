eaf0e55f997edb093d85d4762adde6b86079cabe
RR kodcore <kodcore@protonmail.com>
2024-09-07 15:06:54 -0300

    first commit

---
+++ src/main/java/com/kodcore/todolist/user/UserModel.java
@@ -0,0 +1,12 @@
+package com.kodcore.todolist.user;
+
+import lombok.Data;
+
+@Data
+public class UserModel {
+    
+    private String username;
+    private String name;
+    private String password;
+
+ }
